a1f61c947140972a7ae07bd8df5adef16605de21
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-08-15 16:42:46 -0400

    Corrections to Parser class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,8 +4,10 @@ import shutil
 
 
 class Parser:
+    extensions: List[str] = []
+
     def __init__(self):
-        self.extensions: List[str] = []
+        pass
 
     def valid_extension(self, extension):
         return extension in self.extensions
@@ -31,4 +33,4 @@ class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        self.copy(path, path, source, dest)
+        self.copy(path, source, dest)
